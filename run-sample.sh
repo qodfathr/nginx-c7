6ba87016ff7981569343858f594c605c3ffd4d78
Todd A. Mancini <todd.mancini@daxat.com>
2018-05-29 09:14:20 -0400

    fixed startup script for webhooks

--- run-sample.sh
+++ run-sample.sh
@@ -1,2 +1,3 @@
-docker run -d --name webhooks -p 5455:5000 -v $(pwd)/hooks:/src/hooks:rw -v $(pwd)/webhook-config.json:/app/config.json --privileged=true carlos-jenkins/python-github-webhooks
+#!/bin/bash
+docker run -d --name webhooks -p 5455:5000 -v $(pwd)/hooks:/app/hooks:rw -v $(pwd)/webhook-config.json:/app/config.json --privileged=true qodfathr/python-github-webhooks
 docker run -d -p 1234:80 --privileged=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro local/nginx-webhook
