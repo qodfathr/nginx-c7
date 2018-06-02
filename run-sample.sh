83e7ef4e8775bea50c5ab813376d83cfe51afd61
Todd A. Mancini <todd.mancini@daxat.com>
2018-06-02 17:03:44 -0400

    cleanup

--- run-sample.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run -d --name webhooks -p 5455:5000 -v $(pwd)/hooks:/app/hooks:rw -v $(pwd)/webhook-config.json:/app/config.json --privileged=true qodfathr/python-github-webhooks
-docker run -d -p 1234:80 --privileged=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro local/nginx-webhook
